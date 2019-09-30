21464670c236859880f56f3945a649557fcab00e
J.M. Valverde <regjmlipman@hotmail.com>
2019-09-30 15:19:25 +0300

    generalizing the test for cs3

--- experiments/VoxelInfluenceTest.py
+++ experiments/VoxelInfluenceTest.py
@@ -38,8 +38,7 @@ def main(config, Model, data, base_path, _run):
     firstRange = [10, 10]
     secondRange = [10, 10]
 
-    for ind in range(myrun*1000, (myrun+1)*1000):
-        # TODO
+    for ind in range(15000):
         ind_ = np.unravel_index(ind, (50, 50, 6))
         #t1, t2, t3 = [165, 126, 7]
         # Interest area: 100-150, 100-150, 6-12
